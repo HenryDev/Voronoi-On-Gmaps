48a4c1fe0bf2fcd2226ebb885ee1e49a952e58c9
Henry <henry@Paladin.local>
2015-03-29 14:03:54 -0400

    add d3 class

---
+++ web/D3overlay.dart
@@ -0,0 +1,4 @@
+import 'package:google_maps/google_maps.dart';
+
+class D3overlay extends OverlayView {
+}
--- web/main.dart
+++ web/main.dart
@@ -1,10 +1,12 @@
 import 'dart:html';
 import 'package:google_maps/google_maps.dart';
-import 'package:d3/selection/selection.dart';
+import 'D3overlay.dart';
+
 void main() {
   var mapOptions = new MapOptions()
     ..mapTypeId = MapTypeId.ROADMAP
     ..zoom = 11
     ..center = new LatLng(39.9829514, -82.990829);
-  new GMap(querySelector('#map_canvas'), mapOptions);
+  var map = new GMap(querySelector('#map_canvas'), mapOptions);
+  new D3overlay();
 }
